e5359d994d014bec2d7dba69635131da05c752b4
neethu8989 <nikithafahanas@gmail.com>
2022-12-20 09:01:14 +0300

    commit

---
+++ ecommerse/searchapp/views.py
@@ -0,0 +1,14 @@
+from ecommerseapp.models import Product
+from django.shortcuts import render
+from django.db.models import Q
+
+
+# Create your views here.
+
+def Searchresult(request):
+    products = None
+    query = None
+    if 'q' in request.GET:
+        query = request.GET.get('q')
+        products=Product.objects.all().filter(Q(name__contains=query,) | Q(description__contains=query))
+        return render(request,'search.html',{'query':query,'products':products})
